24c3cef4d4e61250f1c9dee9eefe6c94077ed3c2
Nikos Fytilis <n-fit@live.com>
2017-05-10 13:56:58 +0300

    probably fine

--- gen3d.sh
+++ gen3d.sh
@@ -4,14 +4,16 @@ main() {
 cd sdf-2d
 echo Finding 2d files
 allthemfiles=( $(find .) )
+echo Found: ${#allthemfiles[@]}
 cd ../sdf-3d
 echo Finding 3d Files
 allthemdonefiles=( $(find .) )
+echo Found: ${#allthemdonefiles[@]}
 cd ..
 echo Removing already computed 3d files from array
 
 #this made me rethink life, our place in the universe and the upcoming singularity
-allthemfiles=$($(echo ${allthemdonefiles[@]} ${allthemfiles[@]} | tr ' ' '\n' | sort | uniq -u ))
+allthemfiles=($(echo "${allthemdonefiles[@]} ${allthemfiles[@]}" | tr ' ' '\n' | sort | uniq -u))
 files=${#allthemfiles[@]}
 divdummy=$(( files / threads ))
 echo Converting sdf input files $files
@@ -27,9 +29,12 @@ wait
 babel_thread() {
 i=$1
 while [ ! "$i" -eq "$2" ]
-do obabel -i sdf ./sdf-2d/"${allthemfiles[$i]}" --gen3d -o sdf\
+do timeout 20s obabel -i sdf ./sdf-2d/"${allthemfiles[$i]}" --gen3d -o sdf\
   -O ./sdf-3d/$(basename "${allthemfiles[$i]}")\
   &>> ./babel-logs/babel-output-gen3d-$date-thread-$thread_i.log
+  if [ ! "$?" == '0' ]
+    then echo "${allthemfiles[$i]}" >> bad_sdfs &
+  fi
   i=$(( i + 1 ))
 done
 echo Thread $thread_i exited
