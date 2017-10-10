f5bb0409405c5c80d9d5d9ee13fadc4b5ce6fcc9
Νικόλαος Κυριάκος Φυτίλης <n-fit@live.com>
2017-10-10 17:52:28 +0300

    added documentation ... in greek

--- zinc15-downloader-dash.sh
+++ zinc15-downloader-dash.sh
@@ -1,11 +1,20 @@
 #! /usr/bin/env dash
-reg_reactive='[A-C]'
-reg_purchasable='[A-B]'
 
+# το shebang και όλο το αρχείο μπορεί να τρέξει και σε bash και dash
+# η οποία είναι πιο γρήγορη αλλά δεν έχει τα ίδια features
+
+# Κάνει print το 28 `"' το οποίο είναι το όνομα του ενός από τα αρχεία
+# που θα κατεβούν
 get_names() {
 cut -d \" -f 28
 }
 
+# Το IFS είναι ουσιαστικά που το for βλέπει για νέο string ie το
+# default είναι το whitespace ' ' και για αυτό δουλεύει το for a s d.
+# Τώρα λέμε στο shell μας να βλέπει ως νέα γραμή κάθε όπου υπάρχοτν τα
+# σύμβολά σε οποιαδήποτε σειρά. Το `cat -` εισάγει στο stdout το stdin
+# που είσαγεται στο function. Τα 2 grep διαχωρίζουν τα αρχεία που θέλουμε
+# με τα παραπάνω κριτήρια.
 json_parser() {
 local IFS='}]['
 for i in $(cat -)
@@ -14,6 +23,9 @@ for i in $(cat -)
 done
 }
 
+# Το τελικό payload (με curl) που στέλνουμε σε μορφή url στην zinc15 με
+# την τελευταία ημ/νια που αποθηκεύεται στο ~/.zinc15-dl-ld. To grep
+# υπάρχει για να αποφευχθεί η εγγραφή λανθασμένων δεδομένων στο τελίκο αρχείο
 get_links() {
 ld_date=$(cat ~/.zinc15-dl-ld)
 date="$(date +%Y-%m-%d | tee ~/.zinc15-dl-ld)"
@@ -24,9 +36,21 @@ curl 'http://zinc15.docking.org/tranches/download'\
 echo The links are in zinc15_$date.sh
 }
 
+# Το αρχικό αρχείο το οποίο κατεβάζουμε από zinc15 και περιέχει όλη την
+# πληροφορία για τα links που θα μας δώσει τελικά
+# (όνομα, αντιδραστικότητα, κλπ).
 get_json() {
 curl 'http://zinc15.docking.org/tranches/all3D.json' --compressed
 }
 
+# [a-z] = regex για όλα τα μικρά γράμματα
+# reactive a-c είναι τα Clean και πάνω
+reg_reactive='[A-C]'
+# ομοίως a-b είναι τα in-stock και πάνω
+reg_purchasable='[A-B]'
+
+# Μερικό string manipulation με το ένα function να δίνει pipe στο άλλο
+# και τελικά να πάει στο tranches
 tranches="$(echo $(get_json | json_parser) | tr ' ' '+')"
+
 get_links
