54caaa324d7df0f17c69259ec5b346c801c2137d
Νικόλαος Κυριάκος Φυτίλης <n-fit@live.com>
2017-12-25 12:04:52 +0200

    reamp gen3d, added pdbqt gen, faster initiliasation

--- gen3d.sh
+++ gen3d.sh
@@ -1,65 +1,34 @@
 #! /usr/bin/env bash
 
 discover() {
-cd sdf-2d
-echo Finding 2d files
-allthemfiles=( $(find .) )
-echo Found: ${#allthemfiles[@]}
-cd ../sdf-3d
-echo Finding 3d Files
-allthemdonefiles=( $(find .) )
-echo Found: ${#allthemdonefiles[@]}
-cd ..
-echo Removing already computed 3d files from array
-#this made me rethink life, our place in the universe and the upcoming singularity
-allthemfiles=($(echo ${allthemdonefiles[@]} ${allthemfiles[@]} | tr ' ' '\n' | sort | uniq -u))
-allthemdonefiles=()
-prefix='./sdf-2d/'
+find sdf-3d sdf-2d -type f -printf "%f\n" |
+sort --parallel=$threads | uniq -u > to_convert
 }
 
 main() {
 [ -e "$1" ] && allthemfiles=( $(cat $1) )
 [ "$allthemfiles" ] || discover
-files=${#allthemfiles[@]}
-divdummy=$(( files / threads ))
-echo Converting sdf input files $files
-lastfile=0
-for thread_i in ${count[@]} last
-  do local thread_files=( ${allthemfiles[@]:$lastfile:$divdummy} )
-  lastfile=$((lastfile + divdummy + 1))
-  babel_thread &
-done
-allthemfiles=()
-wait
-rm /tmp/gen3dpipe*
+parallel -j $threads --bar -I{} babel_thread {.} :::: to_convert
 }
 
 babel_thread() {
-i=0
-total=${#thread_files[@]}
-allthemfiles=()
-while [ "${thread_files[$i]}" ]
-  do for file in ${thread_files[@]:$i:50}
-    do echo $file >> ./babel-logs/babel-output-gen3d-$date-thread-$thread_i.log &
-    timeout 20s obabel -i sdf "$prefix""$file" --gen3d -o sdf\
-    -p 7.4 -O ./sdf-3d/$(basename "$file")\
-    &>> ./babel-logs/babel-output-gen3d-$date-thread-$thread_i.log || echo "$file" >> bad_sdfs
-  done
-  echo Thread $thread_i: $i /$total > /tmp/gen3dpipe$thread_i &
-  i=$(( i + 50 ))
-done
-echo Thread $thread_i exited > /tmp/gen3dpipe$thread_i
+if timeout 20s obabel sdf-2d/"$@".sdf --gen3d -o sdf \
+-p 7.4 -O sdf-3d/"$@".sdf &> /dev/null
+  then echo obabel -i sdf sdf-3d/"$@".sdf -o pdbqt\
+  -O ./pdbqt/$@.pdbqt &> /dev/null
+  else echo $@ >> bad_sdfs
+fi
 }
+export -f babel_thread
 
 sane() {
+[ -e sdf-2d ] || exit 2
 for i in sdf-3d babel-logs
   do [ -e $i ] || mkdir $i
 done
-[ -e sdf-2d ] || exit 2
 [ "$threads" -eq "$threads" ] &> /dev/null ||
 echo Please specify a correct thread count
-count=($(eval echo {$threads..1}))
-date=$(date +%F)
+echo example $(basename $0) 10 for 10 threads
 }
 
 threads=$1
