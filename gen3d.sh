c083106bee2b2cb349d6dc7fd77c2079dc625f36
Nikos Fytilis <n-fit@live.com>
2018-02-11 19:08:51 +0200

    final ?

--- gen3d.sh
+++ gen3d.sh
@@ -2,19 +2,23 @@
 
 discover() {
 find sdf-2d pdbqt -type f ! -name 'Z*' -printf "%f\n" |
+sed -e 's/pdbqt/sdf/g' |
 sort --parallel=$threads | uniq -u > to_convert
 }
 
 main() {
-[ -e "$1" ] && allthemfiles=( $(cat $1) )
-[ "$allthemfiles" ] || discover
+if [ -e "$1" ]
+  then cp "$1" to_convert
+  else discover
+fi
+echo Converting $(wc -l to_convert) files
 parallel -j $threads babel_thread {.} :::: to_convert
 }
 
 babel_thread() {
 if timeout 20s obabel sdf-2d/"$@".sdf --gen3d -o sdf \
 -p 7.4 -O sdf-3d/"$@".sdf &> /dev/null
-  then echo obabel -i sdf sdf-3d/"$@".sdf -o pdbqt\
+  then obabel -i sdf sdf-3d/"$@".sdf -o pdbqt\
   -O ./pdbqt/$@.pdbqt &> /dev/null
   else echo $@ >> bad_sdfs
 fi
--- mysql-q.sh
+++ mysql-q.sh
@@ -33,18 +33,26 @@ help_txt="--min-HBD
 "
 
 mysql_q() {
-mysql --login-path=local -N -e "use BABEL" -e "
-  SELECT IF(Zinc_ext.ID IS NULL,${DB}.ID,Zinc_ext.ID)
-  FROM ${DB}
-  LEFT JOIN Zinc_ext ON Zinc_ext.EXT_ID = ${DB}.ID
-  $(cat -)
-;
-"
+echo Searching $I 1>&2
+if [ ! "$I" = 'Zinc' ]
+  then mysql --login-path=local -N -e "use BABEL" -e "
+    SELECT IF(Zinc_ext.ID IS NULL,${I}.ID,Zinc_ext.ID)
+    FROM ${I}
+    LEFT JOIN Zinc_ext ON ${I}.ID = Zinc_ext.EXT_ID
+    $(cat -)
+  ;
+  "
+  else mysql --login-path=local -N -e "use BABEL" -e "
+    SELECT ID
+    FROM ${I}
+    $(cat -)
+  ;
+  "
+fi
 }
 
 
 query_writer() {
-I=$DB
 for i in HBA1 HBD LogP MW TPSA Positive Negative
   do line_start=WHERE
   [ "$first" ] && line_start=AND
@@ -66,29 +74,31 @@ done
 }
 
 vina_wrapper() {
-if [ "$vina_cfg" ]
-  then out_dir=$(mktemp -d -p .)
-  vina_fun() {
-    vina --cpu 4 --config $vina_cfg --ligand "$1" $vina_rcp --out $out_dir/"$2" &> /dev/null
-  }
-  export vina_cfg vina_log vina_rcp out_dir
-  export -f vina_fun
-  parallel --eta --progress -j $threads vina_fun $db_location/pdbqt/{}.pdbqt {/}.pdbqt
-  score() {
-    mv $@ $(sed '2q;d' $@ | cut -d '-' -f 2 | cut -d ' ' -f1)_$@
-  }
-  export -f score
-  (
-  cd $out_dir
-  find . -type f -printf "%f\n" |
-  parallel --progress -j $threads score {/}
-  find . -type f -printf "%f\n" |
-  head -n $results | tar zcfv ../vina-result_$(date +'%s').tar.gz\
-  --files-from -
-  )
-  rm -rf $out_dir
-  else cat -
-fi
+out_dir=$(mktemp -d -p .)
+vina_fun() {
+vina --cpu 4 --config $vina_cfg --ligand "$1" $vina_rcp --out $out_dir/"$2" &> /dev/null
+}
+export vina_cfg vina_log vina_rcp out_dir
+export -f vina_fun
+echo Starting Docking:
+parallel --eta --progress -j $threads vina_fun $db_location/pdbqt/{}.pdbqt {/}.pdbqt
+score() {
+mv $@ $(sed '2q;d' $@ | cut -d '-' -f 2 | cut -d ' ' -f1)_$@
+}
+export -f score
+(
+cd $out_dir
+echo Renaming results per score:
+find . -type f -printf "%f\n" |
+parallel --progress -j $threads score {/}
+echo Tarring $results best results.
+find . -type f -printf "%f\n" | sort -h | 
+tail -n $results | tar zcfv ../vina-result_$(date +'%s').tar.gz \
+--files-from -
+)
+echo Deleting temporary files
+rm -rf $out_dir #results
+echo Results are in vina-result_$(date +'%s').tar.gz
 }
 
 results=1000
@@ -115,7 +125,7 @@ while true
     --min-Ngtv     ) min_Ngtv=$2 ; shift 2 ;;
     --max-Ngtv     ) max_Ngtv=$2 ; shift 2 ;;
     --help         ) echo "$help_txt" ; exit 2 ;;
-    --db           ) DB=$2 ; shift 2 ;;
+    --db           ) DB+=($2) ; shift 2 ;;
     --zinc-mode    ) zinc_mode=1 ; shift 1 ;;
     --vina-threads ) threads=$((($2+3)/4)) ; shift 2 ;;
     --vina-cfg     ) vina_cfg=$2 ; shift 2 ;;
@@ -126,4 +136,26 @@ while true
   esac
 done
 
-query_writer | mysql_q | vina_wrapper
+db_loop() {
+for I in ${DB[@]}
+  do query_writer | mysql_q
+done
+}
+
+if [ "$vina_cfg" ]
+then if [ -e "./results" ]
+  then read -p "./results found, do you want to use it?" -n 1 -r
+    if [[ $REPLY =~ ^[Nn]$ ]]
+      then echo 'Searching - this could take a lot of time (see mysql threads)'
+      db_loop > results
+    fi
+else
+  echo 'Searching - this could take a lot of time (see mysql threads)'
+  db_loop > results
+  fi
+  echo
+  cat results | vina_wrapper
+else
+  echo 'Searching - this could take a lot of time (see mysql threads)'
+  db_loop > results
+fi
